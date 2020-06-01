7d79f3cc4940359637a1803f652675129089b3d2
Mateusz Rezler <mateuszrezler@gmail.com>
2020-06-01 14:47:20 +0200

    Tidy up the desktop

---
+++ setup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# IMPORTANT! Make your custom modifications before running
+
+# Tidy up the desktop
+# Leave "recyclebin.desktop" and "userfiles.desktop" files only
+mkdir ~/.Desktop
+mv ~/Desktop/computer.desktop ~/.Desktop/computer.desktop
+mv ~/Desktop/helpmanual.desktop ~/.Desktop/helpmanual.desktop
+mv ~/Desktop/network.desktop ~/.Desktop/network.desktop
+mv ~/Desktop/settings.desktop ~/.Desktop/settings.desktop
+

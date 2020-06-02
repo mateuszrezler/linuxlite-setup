41a608827a473842a1a09892664b26807a437217
Mateusz Rezler <mateuszrezler@gmail.com>
2020-06-02 23:49:03 +0200

    Fix webcam problems in Skype

--- setup.sh
+++ setup.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-# IMPORTANT! Make your custom modifications before running
+# IMPORTANT! Before running:
+# 1. Install Skype and Webcam Software using Lite Software tool
+# 2. Make your custom modifications
 
 # Tidy up the desktop
 # Leave "recyclebin.desktop" and "userfiles.desktop" files only
@@ -9,4 +11,12 @@ mv ~/Desktop/computer.desktop ~/.Desktop/computer.desktop
 mv ~/Desktop/helpmanual.desktop ~/.Desktop/helpmanual.desktop
 mv ~/Desktop/network.desktop ~/.Desktop/network.desktop
 mv ~/Desktop/settings.desktop ~/.Desktop/settings.desktop
+
+# Fix webcam problems in Skype
+sudo apt -y install v4l2loopback-dkms
+sudo apt -y install ffmpeg
+echo "sudo modprobe v4l2loopback
+ffmpeg -i /dev/video0 -vcodec rawvideo -pix_fmt yuv420p -vsync 2 -threads 0 \\
+-f v4l2 /dev/video1
+" > ~/.skype.sh
 
